dd91e8bd63db93c9fa506786a5cffa2d406ed57c
guoyanwushu <495197461@qq.com>
2020-09-24 18:18:59 +0800

    测试自动化部署

--- docs/draw/index.js
+++ docs/draw/index.js
@@ -37,7 +37,7 @@ http.createServer(async (req, res) => {
       const projectDir = path.resolve(`./${data.repository.name}`)
      deleteFolderRecursive(projectDir)
      
-      // 拉取仓库最新代码2222233
+      // 拉取仓库最新代码222223344444
       execSync(`git clone https://github.com/guoyanwushu/${data.repository.name}.git ${projectDir}`,{
         stdio:'inherit',
     })
