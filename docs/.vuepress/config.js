e76fdc3f2648e912020df1acce3872a911749b3a
guoyanwushu <495197461@qq.com>
2020-09-24 17:25:59 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Yg Blog',
+  extraWatchFiles: [
+    './**/*.md'
+  ],
+  themeConfig: {
+    nav: [
+      {
+        text: '前端', link: '/front/'
+      }, {
+        text: '绘画', link: '/draw/'
+      }
+    ]
+  }
+}
